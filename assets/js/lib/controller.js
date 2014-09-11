e7be4a77326082044e6cebe66749e6098ce85d06
Christopher Hinstorff <chris@chinstorff.com>
2014-09-11 16:42:18 -0700

    work on controller

--- assets/js/lib/controller.js
+++ assets/js/lib/controller.js
@@ -7,4 +7,57 @@ g.Controller = function () {
 	this.scoresheet.roll([1,1,1,1,1]);
 	return this;
     };
+
+    this.calculateScoresheet = function () {
+	var ret = new g.Scoresheet();
+	var dice = this.scoresheet.getDice();
+	
+	var count = [0, 0, 0, 0, 0, 0];
+	
+	for (var i = 0; i < dice.length; i++) {
+	    count[dice[i]-1] += 1;
+	}
+
+	ret.aces   = count[0] * 1;
+	ret.twos   = count[1] * 2;
+	ret.threes = count[2] * 3;
+	ret.fours  = count[3] * 4;
+	ret.fives  = count[4] * 5;
+	ret.sixes  = count[5] * 6;
+
+	var scount = count.sort(); // works as expected for single digit numbers
+
+	ret.threeOfAKind = scount[5] >= 3 ? dice.reduce(function (a, b) { return a + b }) : 0;
+	ret.fourOfAKind = scount[5] >= 4 ? dice.reduce(function (a, b) { return a + b }) : 0;
+	ret.fullHouse = scount[5] === 3 && scount[4] === 2 ? 25 : 0;
+	ret.smallStraight = 0;
+	ret.largeStright  = 0;
+	ret.yahtzee = scount[5] >= 5 ? 50 : 0;
+	ret.chance = dice.reduce(function (a, b) { return a + b });
+	
+	return ret;
+    };
+};
+
+var maxStraightLength = function (arr) {
+    a = arr.sort();
+
+    var lengths = [0];
+
+    var j;
+    for (var i = 0; i < a.length - 1; i++) {
+	lengths[i] = 0;
+
+	j = i;
+	while (j < a.length) {
+	    if (a[i] === a[j] + j - i) {
+		lengths[i] += 1;
+	    }
+	    else {
+		break;
+	    }
+	}
+    }
+
+    return lengths;
 };
